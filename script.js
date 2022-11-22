729a9087b6f41acb668e1eadd0e7750187c3111d
mertgltkn <117831101+mertgltkn@users.noreply.github.com>
2022-11-22 15:27:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing", {
+    strings:["","Hello World", "Merhaba Dünya", "Hej Verden",  "Hallo Welt", "	こんにちは世界", "	¡Hola Mundo", "Mo ki O Ile Aiye", "Ahoj světe", "Hei maailma", "Bonjour monde", 
+"Γειά σου Κόσμε" ],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
